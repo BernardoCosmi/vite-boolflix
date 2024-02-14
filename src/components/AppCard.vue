dc9f7c81bbdf28ebb146fe3ef37a4e67011588d6
BernardoCosmi <bernardo.daddo90@gmail.com>
2024-02-14 14:43:14 +0100

    feat: poster path display

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -1,6 +1,7 @@
 <template>
-    <div>
-        <h4>
+    <div class="d-flex flex-column col-3 border">
+        <img :src="`https://image.tmdb.org/t/p/w500/${item.poster_path}`" alt="">
+        <h4 class="text-center text-wrap">
             {{item.original_title ? item.original_title : item.original_name}}
         </h4>
     </div>
@@ -12,6 +13,9 @@ import {store} from '../store'
 
 export default{
     name: 'AppCard',
+    props: [
+        'item'
+    ],
     data(){
         return{
             store
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,34 +1,53 @@
 <template>
-    <h2>Film</h2>
-    <AppCard v-for="(movie, index) in store.movies" :key="index" :item="movie"/>
 
-    <h2>Series</h2>
-    <AppCard v-for="(serie, index) in store.series" :key="index" :item="serie"/>
+    <div class="container d-flex flex-column mt-5 mx-auto">
+        <h2 class="text-center">
+            Film
+        </h2>
+        <div class="movies-row row gap-2 align-items-start flex-nowrap">
+            <AppCard v-for="(movie, index) in store.movies" :key="index" :item="movie"/>
+        </div>
+
+        <h2 class="text-center mt-5">
+            Series
+        </h2>
+        <div class="row row gap-2 align-items-start justify-content-center">
+            <AppCard v-for="(serie, index) in store.series" :key="index" :item="serie" />
+        </div>
+    </div>
+    
 </template>
 
 <script>
-import {store} from '../store'
+import { store } from '../store'
 import AppCard from './AppCard.vue'
 
-export default{
+export default {
     name: 'AppHeader',
-    components:{
+    components: {
         AppCard
     },
-    data(){
-        return{
+    data() {
+        return {
             store
         }
     },
-    mounted(){
-        
+    mounted() {
+
     },
-    methods:{
+    methods: {
 
     }
 }
 </script>
 
 <style scoped>
+.container{
+    width: 100vw;
+}
+.movies-row{
+    overflow-x: auto;
+    white-space: nowrap;
 
+}
 </style>
